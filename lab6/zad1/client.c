2d364e647f5b8f9dd93b86de9c26034207922fc9
mirabelka <aga.patro@gmail.com>
2022-04-26 18:38:24 +0200

    nie wypluwa błędu więc chyba jest git

---
+++ lab6/zad1/client.c
@@ -0,0 +1,205 @@
+//
+// Created by mirabelka on 26.04.22.
+//
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <sys/msg.h>
+#include <stdlib.h>
+#include <pwd.h>
+#include <unistd.h>
+#include <string.h>
+
+#define ID 'S'
+#define CLIENT_ID getpid()
+
+#define STOP 1
+#define TO_ALL 2
+#define  TO_ONE 3
+#define LIST 4
+#define INIT 5
+
+typedef struct {
+    long mode;
+    char text[256];
+    pid_t sender;
+    int queue_id;
+    int sender_id;
+    int to_connect_id;
+} message;
+
+
+int client_queue;
+int connected_queue;
+int id;
+bool connected = false;
+int server_queue;
+
+
+void send_message(int queue_id, message* message_){
+    if(msgsnd(queue_id, message_, sizeof(message) - sizeof(long), 0) == -1){
+        printf("cannot send this message\n");
+        exit(1);
+    }
+}
+
+char* get_path(){
+    char* path = getenv("HOME");
+    if (path == NULL){
+        path = getpwuid(getuid()) -> pw_dir;
+    }
+    return path;
+}
+
+void * delete_client(int queue){
+    msgctl(queue, IPC_RMID, NULL);
+}
+
+void receive_message(int queue_id, message* message_, long type){
+    if(msgrcv(queue_id, message_, sizeof(message) - sizeof(long), message_->mode, 0) == -1){
+        printf("cannot receive message\n");
+        exit(1);
+    }
+}
+
+void send_STOP(){
+    printf("client sent STOP\n");
+    message message_;
+    send_message(server_queue, &message_);
+    msgctl(client_queue, IPC_RMID, NULL);
+    exit(0);
+}
+
+void receive_message_nowait(int queue_id, message* message_, long mode){
+    if (msgrcv(queue_id, message_, sizeof(message) - sizeof(long ), mode, IPC_NOWAIT) == -1){
+        printf("cannot receive message");
+        exit(1);
+    }
+}
+
+bool check(){
+    struct timeval timeval_;
+    fd_set fds;
+    timeval_.tv_sec = 1;
+    timeval_.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    select(STDIN_FILENO + 1, &fds, NULL, NULL, &timeval_);
+    return FD_ISSET(0, &fds);
+}
+
+void to_one(int current_id){
+    char buffer0[256];
+    char buffer1[256];
+    printf("give me message to pass:\n");
+    scanf("%c", buffer0);
+    printf("give me id another user:\n");
+    scanf("%c", buffer1);
+    int id = atoi(buffer1);
+    message message_;
+    message_.mode = TO_ONE;
+    message_.sender_id = current_id;
+    message_.queue_id = id;
+    strcpy(message_.text, buffer0);
+    printf("message was sent\n");
+}
+
+bool is_empty(int queue){
+    struct msqid_ds buffer;
+    msgctl(queue, IPC_STAT, &buffer);
+    if (buffer.msg_qnum != 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+void catch(){
+    while(!is_empty(client_queue)){
+        message message_;
+        receive_message_nowait(client_queue, &message_, 0);
+        switch (message_.mode) {
+            case STOP:
+//                handle_STOP();
+                exit(0);
+
+        }
+    }
+}
+void send_list(){
+    printf("client sent list\n");
+    message message_ = {.mode = LIST};
+    send_message(server_queue, &message_);
+}
+
+void to_all(int id){
+    message message_;
+    char buffer[256];
+    message_.mode = TO_ALL;
+    message_.queue_id = id;
+    printf("give me message to pass:\n");
+    scanf("%c", buffer);
+    strcpy(message_.text, buffer);
+    printf("send all messages!\n");
+}
+
+int main (){
+    printf("hi, im client\n");
+    signal(SIGINT, send_STOP);
+    atexit(delete_client(client_queue));
+    key_t server_key = ftok(get_path(), ID);
+    if (server_key == -1){
+        printf("cannot generate key\n");
+        return -1;
+    }
+    server_queue = msgget(server_key, 0);
+    if (server_queue == -1){
+        printf("cannot create server queue\n");
+        return -1;
+    }
+    printf("server queue: %d\n", server_queue);
+    key_t client_key = ftok(get_path(), CLIENT_ID);
+    if (client_key == -1){
+        printf("cannot create client key\n");
+        return -1;
+    }
+    client_queue = msgget(client_key, IPC_CREAT | IPC_EXCL | 0666);
+    if (client_queue == -1){
+        printf("cannot create client queue\n");
+        return -1;
+    }
+    printf("client queue id: %d", client_queue);
+
+    message message_ = {.queue_id = client_queue, .mode = INIT};
+    send_message(server_queue, &message_);
+    message received_message;
+    receive_message(client_queue, &received_message, INIT);
+    printf("--client id--\n%d\n\n", received_message.sender_id);
+    id = received_message.sender_id;
+
+    int id_to_connect;
+    char buffer[256];
+
+    while(true){
+        if (check()){
+            fgets(buffer, 256, stdin);
+            if (strcmp("LIST\n", buffer) == 0){
+                send_list();
+            }else if(strcmp("STOP\n", buffer) == 0){
+                send_STOP();
+                exit(0);
+            }else if(strcmp("ALL\n", buffer) == 0){
+                to_all(client_queue);
+            }else if(strcmp("ONE\n", buffer) == 0){
+                to_one(client_queue);
+            } else {
+                printf("i got the message");
+            }
+
+        }
+        sleep(1);
+//        catch();
+    }
+
+}
---
+++ lab6/zad1/server.c
@@ -0,0 +1,185 @@
+//
+// Created by mirabelka on 26.04.22.
+//
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdlib.h>
+#include <pwd.h>
+#include <signal.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#define ID 'S'
+
+#define STOP 1
+#define TO_ALL 2
+#define  TO_ONE 3
+#define LIST 4
+#define INIT 5
+
+typedef struct {
+    long mode;
+    char text[256];
+    pid_t sender;
+    int queue_id;
+    int sender_id;
+    int to_connect_id;
+} message;
+
+typedef struct {
+    int id;
+    int queue_id;
+    int connection_id;
+    bool connected;
+    bool available;
+} client;
+
+client clients[10];
+int server_queue;
+
+
+void send_message(int queue_id, message* message_){
+    if(msgsnd(queue_id, message_, sizeof(message) - sizeof(long), 0) == -1){
+        printf("cannot send this message\n");
+        exit(1);
+    }
+}
+
+void receive_message(int queue_id, message* message_, long type){
+    if(msgrcv(queue_id, message_, sizeof(message) - sizeof(long), message_->mode, 0) == -1){
+        printf("cannot receive message\n");
+        exit(1);
+    }
+}
+
+void delete_server(int queue){
+    msgctl(queue, IPC_RMID, NULL);
+}
+
+char* get_path(){
+    char* path = getenv("HOME");
+    if (path == NULL){
+        path = getpwuid(getuid()) -> pw_dir;
+    }
+    return path;
+}
+
+void handle_SIGINT(){
+    printf("\nserver received SIGINT\n");
+    exit(0);
+}
+
+void stop_server(){
+    printf("stopping server");
+    message message_;
+    int client_queue;
+    for (int i = 0; i < 10; i++){
+        if (clients[i].connected){
+            printf("stopping client, id: %d\n", i);
+            client_queue = clients[i].queue_id;
+            message_.mode = STOP;
+            send_message(client_queue, &message_);
+            receive_message(server_queue, &message_, STOP);
+            printf("STOP received form client\n");
+        }
+    }
+    delete_server(server_queue);
+    printf("as you wanted, i've just stopped the server!");
+}
+
+void stop_sender(message* message_){
+    printf("\nserver received STOP\n");
+    message_->mode = STOP;
+    int client_id = message_->sender_id;
+    clients[client_id].connected = false;
+}
+
+void to_all(message* message_){
+    int current_client = message_->sender_id;
+
+    for (int i = 0; i < 10; i++){
+        if (clients[i].id != 0 && clients[i].id != current_client){
+            printf("sent message to: %d\n", clients[i].id);
+            message new_message;
+            sprintf(new_message.text, "%s", message_->text);
+            new_message.sender_id = clients[i].id;
+        }
+    }
+}
+
+void to_one(message* message_){
+    int current_client = message_->sender_id;
+    message new_message;
+    sprintf(new_message.text, "%s", message_->text);
+    new_message.sender = current_client;
+    printf("message sent\n");
+}
+
+void list_clients(message* message_){
+    printf("-----------------------------");
+    printf("listing active clients:\n");
+    for (int i = 0; i < 10; i++){
+        if (clients[i].available){
+            printf("id: %d, queue id: %d, status: available\n", clients[i].id, clients[i].queue_id);
+        }
+    }
+    printf("-----------------------------");
+}
+
+void pick_up_message(message* message_){
+    switch (message_->mode) {
+        case STOP:
+            stop_sender(message_);
+            break;
+        case TO_ALL:
+            to_all(message_);
+            break;
+        case TO_ONE:
+            to_one(message_);
+            break;
+        case LIST:
+            list_clients(message_);
+            break;
+        default:
+            printf("error with handling this message!\n");
+            exit(1);
+    }
+}
+
+int main(int args, char* argv[]){
+    printf("hi, im server!");
+    atexit(stop_server);  // ustawia ze ta funkcja ma sie wykonac przed zakonczeniem programu
+    key_t key = ftok(get_path(), ID);
+    if (key == -1){
+        printf("cannot create queue\n");
+        return -1;
+    }
+    server_queue = msgget(key, IPC_CREAT | IPC_EXCL | 0666);
+    if (server_queue == -1){
+        printf("cannot create queue\n");
+        return -1;
+    }
+    printf("server queue id: %d\n", server_queue);
+    printf("adding client... \n");
+    for (int i = 0; i < 10; i++){
+        clients[i].available = true;
+        clients[i].connected = false;
+    }
+    printf("all clients added!\n");
+    signal(SIGINT, handle_SIGINT);
+
+    message message_;
+    while (true){
+        int message_size = sizeof(message) - sizeof(message_.mode);
+        if (msgrcv(server_queue, &message_, message_size, -INIT-1, 0)){
+            printf("error with received maessage");
+            return -1;
+        }
+        pick_up_message(&message_);
+    }
+
+}
